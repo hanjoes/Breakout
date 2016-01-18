c3dcda6594b1fe1a496d3d8f432262c7c41d49ad
hanjoes <hanzhou87@gmail.com>
2016-01-18 15:05:43 +0800

    Added a specific controller for the main game scene with a generic UIView.

---
+++ Breakout/Breakout/BreakoutViewController.swift
@@ -0,0 +1,32 @@
+//
+//  BreakoutViewController.swift
+//  Breakout
+//
+//  Created by Hanzhou Shi on 1/18/16.
+//  Copyright © 2016 USF. All rights reserved.
+//
+
+import UIKit
+
+class BreakoutViewController: UIViewController {
+    @IBOutlet weak var gameView: UIView!
+
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
